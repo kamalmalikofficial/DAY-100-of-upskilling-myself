115d7f9da71d4a4fcce192611220304d49c324e7
Kamal Malik <kamalmalik.krw@gmail.com>
2024-12-18 23:35:25 +0530

    Add files via upload

---
+++ 73 Set Matrix Zeroes.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<vector<int>> matrix = {{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+    int n = matrix.size(); 
+    int m = matrix[0].size(); 
+    vector<int> row(n, 1); 
+    vector<int> col(m, 1); 
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] == 0) {
+                row[i] = 0;
+                col[j] = 0;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (row[i] == 0 || col[j] == 0) {
+                matrix[i][j] = 0;
+            }
+        }
+    }
+    
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cout<<matrix[i][j]<<" ";
+        }
+        cout<<"\n";
+    }
+    
+    
+    return 0;
+}
